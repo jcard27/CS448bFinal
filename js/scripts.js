e5667eb76470a49b17a64ffc608a1d654e2812fc
Jen Cardona <jenniferleecardona@gmail.com>
2018-12-03 15:56:52 -0800

    have first scatterplot with tooltips and unlinked dropdown

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
+
+
+
 // Add an SVG element to the DOM
-var svg = d3.select('body').append('svg')
+var svg = d3.select('#plot1div').append('svg') //d3.select('body').append('svg')
   .attr('width', screen.width)
   .attr('height', screen.height);
 
@@ -9,11 +12,11 @@ d3.csv("/data/filtered_finalProjectDataset.csv", parseInputRow).then(loadData);
 //Parse CSV rows and returns array of objects with the specified fields.
 function parseInputRow (d) {
     return {
-       name : d.FCID_desc,
+       name : d.FCID_Desc,
        portion_desc : d. Portion_description,
        ghge_portion : +d.kgCO2perPortion,
        kcal_portion : +d.kcalPortion,
-       protein_portion : +d.protein_portion,
+       protein_portion : +d.proteinPortion,
        carbs_portion : +d.carbsPortion,
        fat_portion : +d.fatPortion,
        sugar_portion : +d.sugarPortion,
@@ -21,10 +24,10 @@ function parseInputRow (d) {
        veg : +d.Veg,
        meat : +d.Meat,
        dairy : +d.Dairy,
-       grain : +d.grain,
-       protein : +d.protein,
-       oil : +d.oil,
-       beverage : +d.beverage
+       grain : +d.Grain,
+       protein : +d.Protein,
+       oil : +d.Oil,
+       beverage : +d.Beverage
 
    };
 };
@@ -32,353 +35,92 @@ function parseInputRow (d) {
 //Callback for d3.csv (all data related tasks go here)
 function loadData(loadedData){
    csvData = loadedData
-   console.log(csvData)
-   //generateVis(csvData);
+   generateVis(csvData);
 };
 
 //Generate visualization using parsed data from CSV (array of objects)
 function generateVis(csvData){
   ///////////////// Declaring Vars /////////////////////
-    //Groups for viz elements in svg container
-    var cursorGroupA =	svg.append("g"); // group for POI A draggable point
-    var cursorGroupB =	svg.append("g"); // group for POI B draggable point
-    var backgroundGroup = svg.append('g'); //group for greyed out restaurants
-    var plotGroup = svg.append('g'); // group for highlighted restaurants
-    var sliderA =	svg.append("g"); // group for slider to adjust radius around A
-    var sliderB =	svg.append("g"); // group for slider to adjust radius around B
-    var sliderScore =	svg.append("g"); // group for slider to adjust score threshold for filter
-    var annotationGroup = svg.append("g");// group for annotations
 
-    //Draggable POI variables
-    var posA = [{x: 100, y: 250}]; // Initial position for A px
-    var posB = [{x: 250, y: 250}]; // Initial position for B px
-    var rMilesA = 1; // Initial radius around point A in miles
-    var rMilesB = 1; // Initial radius around point B in miles
-    var degreesPerPixel = projection.invert([1,1])[0] - projection.invert([2,1])[0];//change in degrees per 1 pixel change in longitude
-    var rPxA = calcPxRadius(rMilesA, degreesPerPixel); // radius around A in pixels
-    var rPxB = calcPxRadius(rMilesB, degreesPerPixel); // radius around B in pixels
-    var outerCircleA = cursorGroupA.append("circle"); // circle showing radius around A
-    var innerCircleA = cursorGroupA.append("circle"); // circle showing POI A
-    var outerCircleB = cursorGroupB.append("circle"); // circle showing radius around B
-    var innerCircleB = cursorGroupB.append("circle"); // circle showing POI B
+  //For dropdown code: https://bl.ocks.org/ProQuestionAsker/8382f70af7f4a7355827c6dc4ee8817d
+  var dropdown1 = d3.select("#dropdown1div")
+  dropdown1.append("select")
+           .selectAll("option")
+           .data(csvData)
+           .enter()
+           .append("option")
+           .attr("value", function(d){return d.name;})
+           .text(function(d){return d.name;})
 
-    //Slider Variables
-    var xMin = 650 + 20; // x-position to start at
-    var xMax = xMin + 100; // x-position to end at
-    var ySliderA = 250; // y-position of slider adjusting radius A
-    var ySliderB = ySliderA + 70; // y-position of slider adjusting B
-    var ySliderScore = ySliderB + 70; // y-position of slider adjusting score filter
-    var sliderTitleOffset = 20;
-    var sliderMileRange = 4; // maximum radius around POI
-    var sliderScoreRange = 29; // range between minimum and maximum score
-    var MINSCORE = 71
-    var minimumScore = MINSCORE; // minumum score for filter
-    var sliderAPos = [-(rMilesA*(xMin - xMax)/sliderMileRange - xMin)]; // x-position of slider marker
-    var sliderBPos = [-(rMilesB*(xMin - xMax)/sliderMileRange - xMin)]; // x-position of slider marker
-    var sliderScorePos = [-((minimumScore-MINSCORE)*(xMin - xMax)/sliderScoreRange - xMin)]; // x-position of slider marker
-    var currentRadius = sliderA.append("text"); // text displaying the radius
-    var sliderACircle = sliderA.append("circle"); // circle marking slider position
-    var currentRadiusB = sliderB.append("text"); // text displaying the radius
-    var sliderBCircle = sliderB.append("circle"); // circle marking slider position
-    var currentMinScore = sliderB.append("text"); // text displaying score to filter by
-    var sliderScoreCircle = sliderScore.append("circle"); // circle marking slider position
+  let plotMargin = 100;
+  let plotWidth = 500; //500;
+  let plotHeight = 500; //500;
 
-    // Variable containing data filtered by the score requirement
-    var filteredPoints = csvData;
-
-/////////// Draw all restaurants initially greyed out ///////
-    updateGreyRestaurants(filteredPoints, backgroundGroup);
-
-  ////////////// Draw sliders for user inputs ///////////////
-  // sliderA.append('rect')
-  //        .attr("x", xMin - 30)
-  //        .attr("y", ySliderA - 70)
-  //        .attr("width", 500)
-  //        .attr("height", 350)
-  //        .style("fill", "white")
-    // Slider for radius of POI A
-    sliderA.append("line")
-          .attr("x1", xMin)
-          .attr("x2", xMax)
-          .attr("y1", ySliderA)
-          .attr("y2", ySliderA)
-          .attr("class", "sliderBar")
-    sliderA.append("text")
-          .attr("class", "sliderNumber")
-          .attr("x",	xMin - 20)
-          .attr("y",	ySliderA + 7)
-          .text("0")
-    sliderA.append("text")
-          .attr("class", "sliderNumber")
-          .attr("x",	xMax + 20)
-          .attr("y",	ySliderA + 7)
-          .text(sliderMileRange)
-    sliderA.append("text")
-          .attr("class", "sliderTitle")
-          .attr("x",	xMin - 20)
-          .attr("y",	ySliderA - sliderTitleOffset)
-          .text("Radius in miles around point A")
-    annotationGroup.append("circle")
-                   .attr("class", "POI")
-                   .style("fill", "green")
-                   .attr("cx", xMin + 200)
-                   .attr("cy", ySliderA - sliderTitleOffset - 6)
-    currentRadius
-          .attr("class", "sliderNumber")
-          .attr("x",	sliderAPos[0])
-          .attr("y",	ySliderA + 25)
-          .text(rMilesA)
-    sliderACircle
-          .attr("class", "sliderCircle")
-          .attr("cx", sliderAPos)
-          .attr("cy", ySliderA)
-          .call(d3.drag().on("drag",	update_sliderA));
+  let plot1 = svg.append('g')
+                 .attr('transform', `translate(${plotMargin}, ${plotMargin})`);
+  let xScale = d3.scaleLinear()
+                 .domain([0,400])
+                 .range([0, plotWidth]);
+  let yScale = d3.scaleLinear()
+                .domain([0,3.5])
+                .range([plotHeight, 0]);
+  let xAxis = plot1.append('g')
+                  .attr('transform', `translate(0, ${plotHeight})`)
+                  .call(d3.axisBottom(xScale));
+  let yAxis = plot1.append('g')
+                  .call(d3.axisLeft(yScale));
+  let annotationGroup = plot1.append('g');
 
 
 
-    // Slider for radius of POI B
-    sliderB.append("line")
-          .attr("class", "sliderBar")
-          .attr("x1", xMin)
-          .attr("x2", xMax)
-          .attr("y1", ySliderB)
-          .attr("y2", ySliderB)
-    sliderB.append("text")
-          .attr("class", "sliderNumber")
-          .attr("x",	xMin - 20)
-          .attr("y",	ySliderB + 7)
-          .text("0")
-    sliderB.append("text")
-          .attr("class", "sliderNumber")
-          .attr("x",	xMax + 20)
-          .attr("y",	ySliderB + 7)
-          .text(sliderMileRange)
-    sliderB.append("text")
-          .attr("class", "sliderTitle")
-          .attr("x",	xMin - 20)
-          .attr("y",	ySliderB - sliderTitleOffset)
-          .text("Radius in miles around point B")
-    annotationGroup.append("circle")
-                   .attr("class", "POI")
-                   .style("fill", "blue")
-                   .attr("cx", xMin + 200)
-                   .attr("cy", ySliderB - sliderTitleOffset - 6)
-    currentRadiusB
-          .attr("class", "sliderNumber")
-          .attr("x",	sliderBPos[0])
-          .attr("y",	ySliderB + 25)
-          .text(rMilesB)
-    sliderBCircle
-          .attr("class", "sliderCircle")
-          .attr("cx", sliderBPos)
-          .attr("cy", ySliderB)
-          .call(d3.drag().on("drag",	update_sliderB));
+  addCircles(csvData, plot1, "plot1AllCircles", xScale, yScale);
+
+  //Add circles to plot1
+  function addCircles(filteredPoints, plot, circleClass, xScale, yScale) {
+    var circles = plot.selectAll("circle")
+                    .data(filteredPoints)
+     circles.enter().append("circle")
+        .merge(circles)
+        .attr("class", circleClass)
+        .attr("cx", function (d) {return xScale(d.kcal_portion);})
+        .attr("cy", function (d) {return yScale(d.ghge_portion);})
+     circles.exit().remove();
+     plot.selectAll("circle")
+                    .on("mouseover",	displayName)
+                    .on("mouseout", undisplay);
+  };
+
+  // Highights circle with stroke and displays restaurant name
+  function displayName (d) {
+    var xPos = this.cx.animVal.value;
+    var yPos = this.cy.animVal.value;
+    var name = d3.select(this).datum().name;
+    // var score = d3.select(this).datum().inspection_score;
+    var highlightedPoint = d3.select(this)
+                             .attr("r", 4)
+                             .style("stroke", "black")
+                             .style("stroke-width", "2");
+      annotationGroup.append("text")
+                     .attr("x", xPos - 4)
+                     .attr("y", yPos - 22)
+                     .attr("class", "annotations")
+                     .text(name);
+      // annotationGroup.append("text")
+      //                .attr("x", xPos - 4)
+      //                .attr("y", yPos - 8)
+      //                .attr("class", "annotations")
+      //                .text("Inspection score: " + score);
+  };
+
+  // Undoes the highlighting from displayName (removes stroke and annotation)
+  function undisplay (d) {
+    d3.select(this)
+      .attr("r", 2)
+      .style("stroke-width", "0")
+    annotationGroup.selectAll("text").remove();
+  }
 
 
-
-    // Slider for filtering visible restaurants by inspection score
-    sliderScore.append("line")
-          .attr("class", "sliderBar")
-          .attr("x1", xMin)
-          .attr("x2", xMax)
-          .attr("y1", ySliderScore)
-          .attr("y2", ySliderScore)
-    sliderScore.append("text")
-          .attr("class", "sliderNumber")
-          .attr("x",	xMin - 20)
-          .attr("y",	ySliderScore + 7)
-          .text(MINSCORE)
-    sliderScore.append("line")
-          .attr("x1", xMin)
-          .attr("x2", xMin)
-          .attr("y1",	ySliderScore)
-          .attr("y2",	ySliderScore + 7)
-    sliderScore.append("text")
-          .attr("class", "sliderNumber")
-          .attr("x",	xMax + 20)
-          .attr("y",	ySliderScore + 7)
-          .text(sliderScoreRange + MINSCORE)
-  sliderScore.append("text")
-        .attr("class", "sliderTitle")
-        .attr("x",	xMin - 20)
-        .attr("y",	ySliderScore - sliderTitleOffset)
-        .text("Minimum Inspection Score*")
-   currentMinScore
-          .attr("class", "sliderNumber")
-          .attr("x",	sliderScorePos[0])
-          .attr("y",	ySliderScore + 25)
-          .text(minimumScore)
-    sliderScoreCircle
-          .attr("class", "sliderCircle")
-          .attr("cx", sliderScorePos)
-          .attr("cy", ySliderScore)
-          .call(d3.drag().on("drag",	update_sliderScore));
-
-   ////////////// Draw POIs A and B and surrounding radii ///////////////
-     // POI A
-     outerCircleA = cursorGroupA.append("circle")
-                 .attr("class", "outerRadius")
-                 .style("fill",	"#009900")
-                 .style("stroke", 1)
-                 .attr("r",	rPxA)
-                 .attr("cx",	posA[0].x)
-                 .attr("cy",	posA[0].y)
-                 .call(d3.drag().on("drag", update_A));
-     innerCircleA = cursorGroupA.append("circle")
-                 .attr("class", "POI")
-                 .attr("fill", "#009900")
-                 .attr("cx",	posA[0].x)
-                 .attr("cy",	posA[0].y)
-                 .call(d3.drag().on("drag",	update_A));
-
-     // POI B
-     outerCircleB
-                 .attr("class", "outerRadius")
-                 .style("fill",	"blue")
-                 .attr("r",	rPxB)
-                 .attr("cx",	posB[0].x)
-                 .attr("cy",	posB[0].y)
-                 .call(d3.drag().on("drag", update_B));
-     innerCircleB = cursorGroupB.append("circle")
-                 .attr("class", "POI")
-                 .attr("fill", "blue")
-                 .attr("cx",	posB[0].x)
-                 .attr("cy",	posB[0].y)
-                 .call(d3.drag().on("drag", update_B));
-
-     //////////// Event Callbacks for Updates ////////////////
-     //Update viz when slider is dragged
-     function update_sliderA(d) {
-       rMilesA = moveSlider (d3.event,
-                                  sliderAPos,
-                                  xMin,
-                                  xMax,
-                                  sliderMileRange,
-                                  sliderACircle,
-                                  currentRadius,
-                                  0)
-         rPxA = calcPxRadius(rMilesA, degreesPerPixel);
-         outerCircleA.attr("r", rPxA)
-         closePoints = getPoints(filteredPoints, rPxA, rPxB, posA, posB)
-         updateRestaurants(closePoints, plotGroup)
-         return rPxA
-     };
-
-     //Update viz when slider is dragged
-     function update_sliderB(d) {
-       rMilesB = moveSlider (d3.event,
-                                  sliderBPos,
-                                  xMin,
-                                  xMax,
-                                  sliderMileRange,
-                                  sliderBCircle,
-                                  currentRadiusB,
-                                  0)
-         rPxB = calcPxRadius(rMilesB, degreesPerPixel);
-         outerCircleB.attr("r", rPxB)
-         closePoints = getPoints(filteredPoints, rPxA, rPxB, posA, posB)
-         updateRestaurants(closePoints, plotGroup)
-     };
-
-     //Update viz when slider is dragged
-     function update_sliderScore(d) {
-         minimumScore = moveSlider (d3.event,
-                                    sliderScorePos,
-                                    xMin,
-                                    xMax,
-                                    sliderScoreRange,
-                                    sliderScoreCircle,
-                                    currentMinScore,
-                                    MINSCORE)
-        currentMinScore.attr("x", sliderScorePos)
-                       .text(Math.round(minimumScore))
-         filteredPoints = filterByScore(csvData, minimumScore)
-         updateGreyRestaurants(filteredPoints, backgroundGroup)
-         closePoints = getPoints(filteredPoints, rPxA, rPxB, posA, posB)
-         updateRestaurants(closePoints, plotGroup)
-     };
-
-     //Update viz when POI A is dragged
-     function update_A(d) {
-         //instructionGroup.selectAll("text").remove();
-         cursorGroupA.selectAll("circle")
-            .attr("cx", d3.event.x)
-            .attr("cy", d3.event.y);
-         posA = [{x: d3.event.x, y: d3.event.y}];
-         closePoints = getPoints(filteredPoints, rPxA, rPxB, posA, posB)
-         updateRestaurants(closePoints, plotGroup)
-     };
-
-     //Update viz when POI B is dragged
-     function update_B(d) {
-      // instructionGroup.selectAll("text").remove();
-       cursorGroupB.selectAll("circle")
-          .attr("cx", d3.event.x)
-          .attr("cy", d3.event.y);
-         posB = [{x: d3.event.x, y: d3.event.y}];
-         closePoints = getPoints(filteredPoints, rPxA, rPxB, posA, posB)
-         updateRestaurants(closePoints, plotGroup)
-     };
-
-     //Add red circles for all restaurants based on long and lat
-     function updateRestaurants(closePoints, plotGroup) {
-        var circles = plotGroup.selectAll("circle")
-                        .data(closePoints)
-        circles.enter().append("circle")
-           .merge(circles)
-           .attr("class", "highlightedRestaurants")
-           .attr("cx", function (d) {return d.proj[0];})
-           .attr("cy", function (d) {return d.proj[1];})
-        circles.exit().remove();
-        plotGroup.selectAll("circle")
-                 .on("mouseover",	displayName)
-                 .on("mouseout", undisplay);
-     };
-
-     //Add green circles for restaurants that meet the score requirement
-     function updateGreyRestaurants(filteredPoints, backgroundGroup) {
-       var circles = backgroundGroup.selectAll("circle")
-                       .data(filteredPoints)
-        circles.enter().append("circle")
-           .merge(circles)
-           .attr("class", "mutedRestaurants")
-           .attr("cx", function (d) {return d.proj[0];})
-           .attr("cy", function (d) {return d.proj[1];})
-        circles.exit().remove();
-        backgroundGroup.selectAll("circle")
-                       .on("mouseover",	displayName)
-                       .on("mouseout", undisplay);
-     };
-
-     // Highights circle with stroke and displays restaurant name
-     function displayName (d) {
-       var xPos = this.cx.animVal.value;
-       var yPos = this.cy.animVal.value;
-       var name = d3.select(this).datum().business_name;
-       var score = d3.select(this).datum().inspection_score;
-       var highlightedPoint = d3.select(this)
-                                .attr("r", 4)
-                                .style("stroke", "black")
-                                .style("stroke-width", "2");
-         annotationGroup.append("text")
-                        .attr("x", xPos - 4)
-                        .attr("y", yPos - 22)
-                        .attr("class", "annotations")
-                        .text(name);
-         annotationGroup.append("text")
-                        .attr("x", xPos - 4)
-                        .attr("y", yPos - 8)
-                        .attr("class", "annotations")
-                        .text("Inspection score: " + score);
-     };
-
-     // Undoes the highlighting from displayName (removes stroke and annotation)
-     function undisplay (d) {
-       d3.select(this)
-         .attr("r", 2)
-         .style("stroke-width", "0")
-       annotationGroup.selectAll("text").remove();
-     }
  };
 
 //////////////////////////////////////////////////////////////////////////
