6e18105e77a3684c92b44ead0acbe36e81b3e971
Jen Cardona <jenniferleecardona@gmail.com>
2018-12-06 16:02:44 -0800

    my barchart is on scroller, but the image is doubled for some reason

---
+++ js/sections_modified.js
@@ -0,0 +1,467 @@
+
+/**
+ * scrollVis - encapsulates
+ * all the code for the visualization
+ * using reusable charts pattern:
+ * http://bost.ocks.org/mike/chart/
+ */
+var scrollVis = function () {
+  // constants to define the size
+  // and margins of the vis area.
+  var width = 600;
+  var height = 520;
+  var margin = { top: 0, left: 100, bottom: 40, right: 10 }; // Changes margin between vis and other things
+
+  // Keep track of which visualization
+  // we are on and which was the last
+  // index activated. When user scrolls
+  // quickly, we want to call all the
+  // activate functions that they pass.
+  var lastIndex = -1;
+  var activeIndex = 0;
+
+  // Sizing for the grid visualization
+  var squareSize = 6;
+  var squarePad = 2;
+  var numPerRow = width / (squareSize + squarePad);
+
+  // main svg used for visualization
+  var svg = null;
+
+  // d3 selection that will be used
+  // for displaying visualizations
+  var g = null;
+
+  var xBarScale = d3.scaleBand()
+    .range([0, width]);
+
+  var yBarScale = d3.scaleLinear()
+    .range([0, height]);
+
+  var xAxisBar = d3.axisBottom()
+    .scale(xBarScale);
+
+  // When scrolling to a new section
+  // the activation function for that
+  // section is called.
+  var activateFunctions = [];
+  // If a section has an update function
+  // then it is called while scrolling
+  // through the section with the current
+  // progress through the section.
+  var updateFunctions = [];
+
+  /**
+   * chart
+   *
+   * @param selection - the current d3 selection(s)
+   *  to draw the visualization in. For this
+   *  example, we will be drawing it in #vis
+   */
+  var chart = function (selection) {
+    selection.each(function (rawData) {
+      // create svg and give it a width and height
+      svg = d3.select(this).selectAll('svg').data(rawData)//dataSortedByGHGE);
+
+      var svgE = svg.enter().append('svg');
+      // @v4 use merge to combine enter and existing selection
+      svg = svg.merge(svgE);
+
+      svg.attr('width', width + margin.left + margin.right);
+      svg.attr('height', height + margin.top + margin.bottom);
+
+      svg.append('g');
+
+
+      // this group element will be used to contain all
+      // other elements.
+      g = svg.select('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+      // // // perform some preprocessing on raw data
+      // var wordData = getWords(rawData);
+
+      var dataSortedByGHGE = sortDataByGHGE(rawData);
+      var maxYBar = d3.max(dataSortedByGHGE, function (d) { return d.ghge_portion; });
+      yBarScale.domain([0, maxYBar + 0.05*maxYBar])
+      xBarScale.domain(dataSortedByGHGE.map(function(d){ return d.name; }))
+      // console.log(xBarScale(dataSortedByGHGE[0].name))
+      console.log(yBarScale(1))
+
+      setupVis(dataSortedByGHGE);//(wordData, fillerCounts, histData);
+
+      setupSections();
+    });
+  };
+
+
+  /**
+   * setupVis - creates initial elements for all
+   * sections of the visualization.
+   *
+   * @param wordData - data object for each word.
+   * @param fillerCounts - nested data that includes
+   *  element for each filler word type.
+   * @param histData - binned histogram data
+   */
+  var setupVis = function (dataSortedByGHGE){//wordData, fillerCounts, histData) {
+    // axis
+    g.append('g')
+      .attr('class', 'x axis')
+      .attr('transform', 'translate(0,' + height + ')')
+      .call(xAxisBar);
+    g.select('.x.axis').style('opacity', 0);
+
+    // count openvis title
+    g.append('text')
+      .attr('class', 'title openvis-title')
+      .attr('x', width / 2)
+      .attr('y', height / 3)
+      .text('2013');
+
+    g.append('text')
+      .attr('class', 'sub-title openvis-title')
+      .attr('x', width / 2)
+      .attr('y', (height / 3) + (height / 5))
+      .text('OpenVis Conf');
+
+    g.selectAll('.openvis-title')
+      .attr('opacity', 0);
+
+    // count filler word count title
+    g.append('text')
+      .attr('class', 'title count-title highlight')
+      .attr('x', width / 2)
+      .attr('y', height / 3)
+      .text('180');
+
+    g.append('text')
+      .attr('class', 'sub-title count-title')
+      .attr('x', width / 2)
+      .attr('y', (height / 3) + (height / 5))
+      .text('Filler Words');
+
+    g.selectAll('.count-title')
+      .attr('opacity', 0);
+
+    // // square grid
+    // // @v4 Using .merge here to ensure
+    // // new and old data have same attrs applied
+    // var squares = g.selectAll('.square').data(wordData, function (d) { return d.word; });
+    // var squaresE = squares.enter()
+    //   .append('rect')
+    //   .classed('square', true);
+    // squares = squares.merge(squaresE)
+    //   .attr('width', squareSize)
+    //   .attr('height', squareSize)
+    //   .attr('fill', '#fff')
+    //   .classed('fill-square', function (d) { return d.filler; })
+    //   .attr('x', function (d) { return d.x;})
+    //   .attr('y', function (d) { return d.y;})
+    //   .attr('opacity', 0);
+// console.log(dataSortedByGHGE[0].name)
+console.log(dataSortedByGHGE[0])
+// console.log(dataSortedByGHGE.map(function(d){ return d.name; }))
+    var bars = g.selectAll('.bar').data(dataSortedByGHGE, function(d) {return d.name});
+    var barsE = bars.enter()
+      .append('rect')
+      .attr('class', 'bar');
+    bars = bars.merge(barsE)
+      .attr('x', function(d) { return xBarScale(d.name); })
+      .attr('y', function(d) { return height - yBarScale(d.ghge_portion); })//function (d) {return yBarScale(d.ghge_portion);})
+      .attr('fill', function (d) { return d.color; })
+      .attr('width', xBarScale.bandwidth())
+      .attr('height', 0);
+
+  };
+
+  /**
+   * setupSections - each section is activated
+   * by a separate function. Here we associate
+   * these functions to the sections based on
+   * the section's index.
+   *
+   */
+  var setupSections = function () {
+    // activateFunctions are called each
+    // time the active section changes
+    activateFunctions[0] = showTitle;
+    activateFunctions[1] = showFillerTitle;
+    activateFunctions[2] = showGrid;
+    // activateFunctions[3] = highlightGrid;
+    // activateFunctions[4] = showBar;
+    // activateFunctions[5] = showHistPart;
+    // activateFunctions[6] = showHistAll;
+    // activateFunctions[7] = showCough;
+    // activateFunctions[8] = showHistAll;
+
+    // updateFunctions are called while
+    // in a particular section to update
+    // the scroll progress in that section.
+    // Most sections do not need to be updated
+    // for all scrolling and so are set to
+    // no-op functions.
+    for (var i = 0; i < 9; i++) {
+      updateFunctions[i] = function () {};
+    }
+    // updateFunctions[7] = updateCough;
+  };
+
+  /**
+   * ACTIVATE FUNCTIONS
+   *
+   * These will be called their
+   * section is scrolled to.
+   *
+   * General pattern is to ensure
+   * all content for the current section
+   * is transitioned in, while hiding
+   * the content for the previous section
+   * as well as the next section (as the
+   * user may be scrolling up or down).
+   *
+   */
+
+  /**
+   * showTitle - initial title
+   *
+   * hides: count title
+   * (no previous step to hide)
+   * shows: intro title
+   *
+   */
+  function showTitle() {
+    g.selectAll('.count-title')
+      .transition()
+      .duration(0)
+      .attr('opacity', 0);
+
+    g.selectAll('.openvis-title')
+      .transition()
+      .duration(600)
+      .attr('opacity', 1.0);
+  }
+
+  /**
+   * showFillerTitle - filler counts
+   *
+   * hides: intro title
+   * hides: square grid
+   * shows: filler count title
+   *
+   */
+  function showFillerTitle() {
+    g.selectAll('.openvis-title')
+      .transition()
+      .duration(0)
+      .attr('opacity', 0);
+
+    g.selectAll('.square')
+      .transition()
+      .duration(0)
+      .attr('opacity', 0);
+
+    g.selectAll('.count-title')
+      .transition()
+      .duration(600)
+      .attr('opacity', 1.0);
+
+    g.selectAll('.bar')
+      .transition()
+      .duration(0)
+      .attr('height', 0)
+  }
+
+  /**
+   * showGrid - square grid
+   *
+   * hides: filler count title
+   * hides: filler highlight in grid
+   * shows: square grid
+   *
+   */
+  function showGrid() {
+    g.selectAll('.count-title')
+      .transition()
+      .duration(0)
+      .attr('opacity', 0);
+
+    // g.selectAll('.square')
+    //   .transition()
+    //   .duration(600)
+    //   .delay(function (d) {
+    //     return 5 * d.row;
+    //   })
+    //   .attr('opacity', 1.0)
+    //   .attr('fill', '#ddd');
+
+    g.selectAll('.bar')
+      .transition()
+      .duration(600)
+      .attr('height', function (d) {return yBarScale(d.ghge_portion);})
+  }
+
+
+  /**
+   * DATA FUNCTIONS
+   *
+   * Used to coerce the data into the
+   * formats we need to visualize
+   *
+   */
+
+   function sortDataByGHGE(rawData){
+     sortedData = rawData.sort(function(x, y){
+        return d3.descending(x.ghge_portion, y.ghge_portion);
+     });
+     return sortedData;
+   }
+
+
+  /**
+   * getWords - maps raw data to
+   * array of data objects. There is
+   * one data object for each word in the speach
+   * data.
+   *
+   * This function converts some attributes into
+   * numbers and adds attributes used in the visualization
+   *
+   * @param rawData - data read in from file
+   */
+  // function getWords(rawData) {
+  //   return rawData.map(function (d, i) {
+  //     // is this word a filler word?
+  //     d.filler = (d.filler === '1') ? true : false;
+  //     // time in seconds word was spoken
+  //     d.time = +d.time;
+  //     // time in minutes word was spoken
+  //     d.min = Math.floor(d.time / 60);
+  //
+  //     // positioning for square visual
+  //     // stored here to make it easier
+  //     // to keep track of.
+  //     d.col = i % numPerRow;
+  //     d.x = d.col * (squareSize + squarePad);
+  //     d.row = Math.floor(i / numPerRow);
+  //     d.y = d.row * (squareSize + squarePad);
+  //     console.log(d)
+  //     return d;
+  //   });
+  // }
+
+
+  /**
+   * activate -
+   *
+   * @param index - index of the activated section
+   */
+  chart.activate = function (index) {
+    activeIndex = index;
+    var sign = (activeIndex - lastIndex) < 0 ? -1 : 1;
+    var scrolledSections = d3.range(lastIndex + sign, activeIndex + sign, sign);
+    scrolledSections.forEach(function (i) {
+      activateFunctions[i]();
+    });
+    lastIndex = activeIndex;
+  };
+
+  /**
+   * update
+   *
+   * @param index
+   * @param progress
+   */
+  chart.update = function (index, progress) {
+    updateFunctions[index](progress);
+  };
+
+  // return chart function
+  return chart;
+};
+
+
+/**
+ * display - called once data
+ * has been loaded.
+ * sets up the scroller and
+ * displays the visualization.
+ *
+ * @param data - loaded tsv data
+ */
+function display(data) {
+  // create a new plot and
+  // display it
+  var plot = scrollVis();
+  d3.select('#vis')
+    .datum(data)
+    .call(plot);
+
+  // setup scroll functionality
+  var scroll = scroller()
+    .container(d3.select('#graphic'));
+
+  // pass in .step selection as the steps
+  scroll(d3.selectAll('.step'));
+
+  // setup event handling
+  scroll.on('active', function (index) {
+    // highlight current step text
+    d3.selectAll('.step')
+      .style('opacity', function (d, i) { return i === index ? 1 : 0.1; });
+
+    // activate current section
+    plot.activate(index);
+  });
+
+  scroll.on('progress', function (index, progress) {
+    plot.update(index, progress);
+  });
+}
+
+// // load data and display
+// d3.tsv('data/words.tsv', display);
+
+d3.csv("/data/filtered_finalProjectDataset.csv", parseInputRow).then(loadData);
+
+//Parse CSV rows and returns array of objects with the specified fields.
+function parseInputRow (d) {
+
+    return {
+       name : d.FCID_Desc,
+       portion_desc : d. Portion_description,
+       ghge_portion : +d.kgCO2perPortion,
+       kcal_portion : +d.kcalPortion,
+       protein_portion : +d.proteinPortion,
+       carb_portion : +d.carbsPortion,
+       fat_portion : +d.fatPortion,
+       sugar_portion : +d.sugarPortion,
+       fruit : +d.Fruit,
+       veg : +d.Veg,
+       meat : +d.Meat,
+       dairy : +d.Dairy,
+       grain : +d.Grain,
+       protein : +d.Protein,
+       oil : +d.Oil,
+       beverage : +d.Beverage,
+       color: d.color
+   };
+};
+
+//Callback for d3.csv (all data related tasks go here)
+function loadData(loadedData){
+   var csvData = loadedData
+   csvData.forEach(function(d) {
+                     if (d.fruit > 0) {d.color = color = "#f781bf"
+                   } else if (d.veg > 0) {d.color = color = "#4daf4a"
+                     } else if (d.dairy > 0) {d.color = color = "#377eb8"
+                     } else if (d.meat > 0) {d.color = color = "#e41a1c"
+                   } else if (d.grain > 0) {d.color = color = "#a65628"
+                     } else if (d.oil> 0) {d.color = color = "#ffff33"
+                     } else if (d.protein> 0) {d.color = color = "#ff7f00"
+                     } else if (d.bevarage> 0) {d.color = color = "#a65628"
+                   } else {d.color = "#80b1d3"}
+                   d.num_servings = 0;
+                   })
+   display(csvData);
+};
